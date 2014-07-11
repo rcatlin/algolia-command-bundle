ceefb1f554053f7b61ddeca0dbe4b227c2eae8fc
Ryan Catlin <ryan.catlin@gmail.com>
2014-07-11 14:45:30 -0400

    AddObjectCommand

---
+++ src/AlgoliaCommandBundle/Command/AddObjectCommand.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace AlgoliaCommandBundle\Command;
+
+use AlgoliaSearch\AlgoliaException;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class AddObjectCommand extends AbstractAlgoliaCommand
+{
+    CONST NAME = 'algolia:add-object';
+    CONST ARGUMENT_INDEX = 'index';
+    CONST ARGUMENT_CONTENT = 'content';
+    CONST OPTION_ID = 'id';
+    CONST ERROR_BAD_JSON_MESSAGE = 'Invalid JSON';
+
+    protected function configure()
+    {
+        parent::configure();
+
+        $this->addArgument(self::ARGUMENT_INDEX, InputArgument::REQUIRED, 'Index name.');
+        $this->addArgument(self::ARGUMENT_CONTENT, InputArgument::REQUIRED, 'Object content.');
+        $this->addOption(self::OPTION_ID, self::OPTION_ID, InputOption::VALUE_OPTIONAL, 'Object ID.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $indexName = $input->getArgument(self::ARGUMENT_INDEX);
+        $content = $input->getArgument(self::ARGUMENT_CONTENT);
+        $id = ($input->hasOption(self::OPTION_ID)) ? $input->getOption(self::OPTION_ID) : null;
+
+        $content = json_decode($content, true);
+
+        if (null === $content) {
+            $output->writeln(self::ERROR_BAD_JSON_MESSAGE);
+
+            return AbstractAlgoliaCommand::STATUS_CODE_ERROR;
+        }
+
+        try {
+            $index = $this->client->initIndex($indexName);
+            $result = $index->addObject($content, $id);
+
+            $output->writeln($result);
+        } catch (AlgoliaException $e) {
+            $output->writeln($e->getMessage());
+
+            return AbstractAlgoliaCommand::STATUS_CODE_ERROR;
+        }
+    }
+
+    public function getName()
+    {
+        return self::NAME;
+    }
+}
---
+++ src/AlgoliaCommandBundle/Tests/Command/AddObjectCommandTest.php
@@ -0,0 +1,226 @@
+<?php
+
+namespace AlgoliaCommandBundle\Tests\Command;
+
+use AlgoliaCommandBundle\Command\AbstractAlgoliaCommand;
+use AlgoliaCommandBundle\Command\AddObjectCommand;
+use AlgoliaCommandBundle\Tests\AlgoliaCommandBundleTestCase;
+use AlgoliaSearch\AlgoliaException;
+use AlgoliaSearch\Client;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class AddObjectCommandTest extends AlgoliaCommandBundleTestCase
+{
+    protected function setUp()
+    {
+        $this->output = $this->buildMock('Symfony\Component\Console\Output\OutputInterface');
+        $this->client = $this->buildMock('AlgoliaSearch\Client');
+
+        $this->container = $this->getDefaultContainer($this->apiKey, $this->applicationId);
+
+        $this->command = new AddObjectCommandStub();
+        $this->command->setClient($this->client);
+        $this->command->setContainer($this->container);
+    }
+
+    public function testExecute()
+    {
+        $answer = 'add-object-answer';
+        $indexName = 'add-object-index-name';
+        $id = 'object-id';
+        $content = array(
+            'name' => 'Bill Waterson',
+            'interests' => array(
+                'drawing',
+                'tigers',
+                'the universe',
+                'sledding'
+            )
+        );
+        $jsonContent = json_encode($content);
+        $input = new ArgvInput(
+            array(
+                AddObjectCommand::NAME,
+                $indexName,
+                $jsonContent,
+                '--' . AddObjectCommand::OPTION_ID . '=' . $id,
+            )
+        );
+        $index = $this->buildMock('AlgoliaSearch\Index');
+
+        // Expectations
+        $this->client->expects($this->once())
+            ->method('initIndex')
+            ->with($indexName)
+            ->will($this->returnValue($index))
+        ;
+
+        $index->expects($this->once())
+            ->method('addObject')
+            ->with($content, $id)
+            ->will($this->returnValue($answer))
+        ;
+
+        $this->output->expects($this->once())
+            ->method('writeln')
+            ->with($answer)
+        ;
+
+        // Run command
+        $result = $this->command->run(
+            $input,
+            $this->output
+        );
+
+        // Assertions
+        $this->assertEquals(
+            $result,
+            null
+        );
+    }
+
+    /**
+     * @expectedException RuntimeException
+     * @expectedExceptionMessage Not enough arguments.
+     */
+    public function testExecuteWithoutIndexNameThrowsException()
+    {
+        $input = new ArgvInput(
+            array(
+                AddObjectCommand::NAME
+            )
+        );
+        // Run command
+        $result = $this->command->run(
+            $input,
+            $this->output
+        );
+
+        // Assertions
+        $this->assertEQuals(
+            $result,
+            null
+        );
+    }
+
+    /**
+     * @expectedException RuntimeException
+     * @expectedExceptionMessage Not enough arguments.
+     */
+    public function testExcecuteWithoutContentThrowsException()
+    {
+        $input = new ArgvInput(
+            array(
+                AddObjectCommand::NAME,
+                'index-name'
+            )
+        );
+        // Run command
+        $result = $this->command->run(
+            $input,
+            $this->output
+        );
+
+        // Assertions
+        $this->assertEQuals(
+            $result,
+            null
+        );
+    }
+
+    public function testExcecuteWithBadJson()
+    {
+        $input = new ArgvInput(
+            array(
+                AddObjectCommand::NAME,
+                'index-name',
+                '{"bad":"json"'
+
+            )
+        );
+
+        // Expectations
+        $this->output->expects($this->once())
+            ->method('writeln')
+            ->with(AddObjectCommand::ERROR_BAD_JSON_MESSAGE)
+        ;
+
+        $this->client->expects($this->never())
+            ->method('initIndex')
+        ;
+
+        // Run command
+        $result = $this->command->run(
+            $input,
+            $this->output
+        );
+
+        // Assertions
+        $this->assertEQuals(
+            $result,
+            AbstractAlgoliaCommand::STATUS_CODE_ERROR
+        );
+    }
+
+    public function testExecuteIndexThrowsException()
+    {
+        $indexName = 'index-name';
+        $content = array(
+            'awesome' => 'content'
+        );
+        $input = new ArgvInput(
+            array(
+                AddObjectCommand::NAME,
+                $indexName,
+                json_encode($content)
+            )
+        );
+        $index = $this->buildMock('AlgoliaSearch\Index');
+        $exceptionMessage = 'exception-message';
+
+        // Expectations
+        $this->client->expects($this->once())
+            ->method('initIndex')
+            ->with($indexName)
+            ->will($this->returnValue($index))
+        ;
+
+        $index->expects($this->once())
+            ->method('addObject')
+            ->with($content, null)
+            ->will(
+                $this->throwException(
+                    new AlgoliaException($exceptionMessage)
+                )
+            )
+        ;
+
+        $this->output->expects($this->once())
+            ->method('writeln')
+            ->with($exceptionMessage)
+        ;
+
+        // Run Command
+        $result = $this->command->run($input, $this->output);
+
+        // Assertions
+        $this->assertEquals(
+            $result,
+            AddObjectCommand::STATUS_CODE_ERROR
+        );
+    }
+}
+
+class AddObjectCommandStub extends AddObjectCommand
+{
+    protected function createClient()
+    {
+        return $this->client;
+    }
+
+    public function setClient(Client $client)
+    {
+        $this->client = $client;
+    }
+}
